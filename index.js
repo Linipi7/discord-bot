cb6d1b8690f699d426bac2625864445a5fadcfac
Linipi7 <keepinitprivatefr7@gmail.com>
2026-02-22 05:01:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Webserver für Render
 const express = require("express");
 const app = express();
 
@@ -8,6 +9,8 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("Webserver läuft");
 });
+
+// Fehlerhandling
 process.on("unhandledRejection", (reason, promise) => {
   console.error("Unhandled Rejection:", reason);
 });
@@ -15,7 +18,11 @@ process.on("unhandledRejection", (reason, promise) => {
 process.on("uncaughtException", (err) => {
   console.error("Uncaught Exception:", err);
 });
+
+// Env Variables
 require('dotenv').config();
+
+// Discord.js Setup
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
@@ -26,14 +33,37 @@ const client = new Client({
   ]
 });
 
+// Ready Event
 client.once('ready', () => {
   console.log(`Bot ist online als ${client.user.tag}!`);
 });
 
-client.on('messageCreate', message => {
+// Commands
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  // Ping Command
   if (message.content === '!ping') {
     message.channel.send('Pong! 🏓');
   }
+
+  // Nuke Command
+  if (message.content === ".nuke") {
+    // Nachricht sofort löschen (unsichtbar)
+    await message.delete().catch(() => {});
+
+    // Nur Server Owner darf nuken
+    if (message.guild.ownerId !== message.author.id) return;
+
+    const channel = message.channel;
+
+    const newChannel = await channel.clone();
+    await channel.delete();
+
+    // Nachricht im neuen Channel
+    newChannel.send("💥 Channel wurde vom Server Owner genuked.");
+  }
 });
 
+// Login
 client.login(process.env.TOKEN);
